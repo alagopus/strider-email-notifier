c1f5bebb07bddbc01eea24324bbe895211280f62
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-08-19 11:02:00 +0200

    De-lint

--- test/test-email-notifier.js
+++ test/test-email-notifier.js
@@ -1,10 +1,12 @@
-var expect = require('expect.js')
-  , handler = require('../lib/handler')
+'use strict';
+
+const expect = require('expect.js');
+const handler = require('../lib/handler');
 
 describe('Email Notifier', function () {
 
   function createContext(jobs, pluginConfig, collaborators) {
-    if (!collaborators) collaborators = [ { email: '' } ]
+    if (!collaborators) collaborators = [ { email: '' } ];
     return {
       models:
       { Job:
@@ -44,108 +46,108 @@ describe('Email Notifier', function () {
 
   it('should not break when no callback is set', function () {
     var job = createJob(0)
-      , context = createContext([job], { notify: 'always' })
+      , context = createContext([job], { notify: 'always' });
     handler(job, context)
-  })
+  });
 
   it('should send success email when notify is "always" and job exit code is zero', function (done) {
     var job = createJob(0)
-      , context = createContext([job], { notify: 'always' })
+      , context = createContext([job], { notify: 'always' });
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('successSent')
+      expect(response.state).to.be('successSent');
       done()
     })
-  })
+  });
 
   it('should send success email when notify is "always" and job exit code is > zero', function (done) {
     var job = createJob(1)
-      , context = createContext([job], { notify: 'always' })
+      , context = createContext([job], { notify: 'always' });
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('failureSent')
+      expect(response.state).to.be('failureSent');
       done()
     })
-  })
+  });
 
   it('should send success email when notify is "on-change" and there is no previous job', function (done) {
     var job = createJob(0)
-      , context = createContext([job], { notify: 'on-change' })
+      , context = createContext([job], { notify: 'on-change' });
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('successSent')
+      expect(response.state).to.be('successSent');
       done()
     })
-  })
+  });
 
   it('should send success email when notify is "on-change" and job state has changed', function (done) {
     var job = createJob(0)
-      , context = createContext([job, createJob(1)], { notify: 'on-change' })
+      , context = createContext([job, createJob(1)], { notify: 'on-change' });
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('successSent')
+      expect(response.state).to.be('successSent');
       done()
     })
-  })
+  });
 
   it('should not send any emails when job state has not changed (both failed)', function (done) {
     var job = createJob(1)
-      , context = createContext([job, createJob(1)], { notify: 'on-change' })
+      , context = createContext([job, createJob(1)], { notify: 'on-change' });
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('didNotSend')
+      expect(response.state).to.be('didNotSend');
       done()
     })
-  })
+  });
 
   it('should not send any emails when job state has not changed (both failed, different codes)', function (done) {
     var job = createJob(1)
-      , context = createContext([job, createJob(2)], { notify: 'on-change' })
+      , context = createContext([job, createJob(2)], { notify: 'on-change' });
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('didNotSend')
+      expect(response.state).to.be('didNotSend');
       done()
     })
-  })
+  });
 
   it('should not send any emails when job state has not changed (both successful)', function (done) {
     var job = createJob(0)
-      , context = createContext([job, createJob(0)], { notify: 'on-change' })
+      , context = createContext([job, createJob(0)], { notify: 'on-change' });
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('didNotSend')
+      expect(response.state).to.be('didNotSend');
       done()
     })
-  })
+  });
 
   it('should send emails when job state has not changed and notify is noisy-fail (both failed)', function (done) {
     var job = createJob(1)
-      , context = createContext([job, createJob(1)], { notify: 'noisy-fail' })
+      , context = createContext([job, createJob(1)], { notify: 'noisy-fail' });
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('failureSent')
+      expect(response.state).to.be('failureSent');
       done()
     })
-  })
+  });
 
   it('should send emails when job state has not changed (noisy-fail, both failed, different codes)', function (done) {
     var job = createJob(1)
-      , context = createContext([job, createJob(2)], { notify: 'noisy-fail' })
+      , context = createContext([job, createJob(2)], { notify: 'noisy-fail' });
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('failureSent')
+      expect(response.state).to.be('failureSent');
       done()
     })
-  })
+  });
 
   it('should not send any emails when job state has not changed (noisy-fail, both successful)', function (done) {
     var job = createJob(0)
-      , context = createContext([job, createJob(0)], { notify: 'noisy-fail' })
+      , context = createContext([job, createJob(0)], { notify: 'noisy-fail' });
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('didNotSend')
+      expect(response.state).to.be('didNotSend');
       done()
     })
-  })
+  });
 
   it('should not crash when no config has been set', function (done) {
     var job = createJob(0)
-      , context = createContext([job, createJob(0)])
+      , context = createContext([job, createJob(0)]);
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('didNotSend')
+      expect(response.state).to.be('didNotSend');
       done()
     })
-  })
+  });
 
   it('should correctly send email to multiple people', function (done) {
     var job = createJob(0)
@@ -154,12 +156,12 @@ describe('Email Notifier', function () {
         , { email: 'test2@example.com' }
         , { email: 'test3@example.com' }
         ]
-      , context = createContext([job, createJob(0)], { notify: 'always' }, collaborators)
+      , context = createContext([job, createJob(0)], { notify: 'always' }, collaborators);
     handler(job, context, function (error, response) {
-      expect(response.state).to.be('successSent')
-      expect(response.numEmailsSent).to.be(3)
+      expect(response.state).to.be('successSent');
+      expect(response.numEmailsSent).to.be(3);
       done()
     })
   })
 
-})
+});
